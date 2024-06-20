b8a643bdc73601856c08090e9897d00e3696bf4b
notanewbie <computerchipcity@aol.com>
2024-06-20 09:11:57 -0400

    Update update-plex.sh

--- update-plex.sh
+++ update-plex.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 wget -O "plex-latest.deb" "https://plex.tv/downloads/latest/5?channel=16&build=linux-x86_64&distro=debian&X-Plex-Token=xxxxxxxxxxxxxxxxxxxx"
 sudo dpkg -i "plex-latest.deb"
